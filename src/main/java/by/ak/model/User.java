00010a2e3b23a533382c61df334734e73255ab21
Alexander Karankevich <alexander.karankevich@gmail.com>
2021-11-25 00:43:31 +0300

    remove services remove root resource configure docker-compose.yml update app.yaml add openapi

--- src/main/java/by/ak/model/User.java
+++ src/main/java/by/ak/model/User.java
@@ -2,10 +2,12 @@ package by.ak.model;
 
 import io.quarkus.hibernate.reactive.panache.PanacheEntity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 
 @Entity
 public class User extends PanacheEntity {
+    @Column(length = 40, unique = true)
     private String name;
     private String email;
 }
